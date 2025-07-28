70d1ed0d88016018460eb3fea3adc27a9971ffd3
Konda Sridhar <133839330+sridharkonda628@users.noreply.github.com>
2025-07-28 20:38:30 +0530

    Time: 8 ms (80.38%), Space: 41 MB (84.22%) - LeetHub

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
+        //using Recurion: T.C ==>O(2^n), S.C==> Stack space, Recursive stack calls
+         int maxXor = 0;
+
+        for(int num: nums){
+            maxXor |= num;
+        }
+
+        return cntSubsets(nums, 0,0,maxXor );
+    }
+    public int cntSubsets(int[] nums, int idx, int curr, int maxi){
+        if(idx == nums.length){
+            if(curr == maxi){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+
+        int cntExcludingCurrIdx = cntSubsets(nums,idx+1,curr,maxi);
+        int cntIncludingCurrIdx = cntSubsets(nums,idx+1,curr|nums[idx],maxi);
+
+        return cntExcludingCurrIdx+cntIncludingCurrIdx;
+    }
+}
