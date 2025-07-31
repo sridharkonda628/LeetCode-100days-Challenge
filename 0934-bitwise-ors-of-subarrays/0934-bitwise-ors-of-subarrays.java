945c663fcbafc652001412bb69cd65800c8e9020
Konda Sridhar <133839330+sridharkonda628@users.noreply.github.com>
2025-07-31 19:40:55 +0530

    Update 0934-bitwise-ors-of-subarrays.java

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.java
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.java
@@ -2,13 +2,13 @@ class Solution {
     public int subarrayBitwiseORs(int[] arr) {
         
 
-        int n = arr.length;
+        int n1 = arr.length;
         Set<Integer> subarrayBitwiseORs = new HashSet<>();
        
         Set<Integer> curr = new HashSet<>();
         curr.add(0);
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n1;i++){
             Set<Integer> curr2 = new HashSet<>();
            
            for(int y : curr){
