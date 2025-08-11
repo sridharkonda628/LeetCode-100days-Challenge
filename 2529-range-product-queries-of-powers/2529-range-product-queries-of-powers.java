c517f429c17a055ac008d69e5877212acd0d7270
Konda Sridhar <133839330+sridharkonda628@users.noreply.github.com>
2025-08-11 20:29:37 +0530

    Time: 44 ms (11.83%), Space: 93 MB (30.11%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -1,45 +1,41 @@
 class Solution {
     int mod = (int)1e9+7;
     public int[] productQueries(int n, int[][] queries) {
-    int[] pow = new int[31];
-    // pow[0] = 1;
-    // int stI =0;
-    // int flg =0;
-    for(int i=0;i<30;i++){
-        if(checkBit(i,n)){
-            pow[i] = (1<<i);
-            // if(flg ==0) {
-            //     stI = i;
-            //     flg =1;
-            // }
+        int[] pow = new int[31];
+    
+        for(int i=0;i<30;i++){
+            if(checkBit(i,n)){
+                pow[i] = (1<<i);
+            
+            }
+        
         }
-        // pow[i] = pow[i-1]*2;
-    }
-    List<Integer> compPow= new ArrayList<>();
-    for(int num: pow){
-        if(num!=0){
-            compPow.add(num);
+
+        List<Integer> compressedPowerArray= new ArrayList<>();
+        for(int num: pow){
+            if(num!=0){
+                compressedPowerArray.add(num);
+            }
         }
-    }
 
         int len = queries.length;
 
-        int []res = new int[len];
+        int []productQueries = new int[len];
         int idx =0;
         for(int[] que: queries){
             int st = que[0];
             int end = que[1];
             long tp =1;
             for(int i=st;i<=end;i++){
-                tp=(tp%mod*compPow.get(i)%mod)%mod;
+                tp=(tp%mod*compressedPowerArray.get(i)%mod)%mod;
             }
-            res[idx++] = (int)tp;
+            productQueries[idx++] = (int)tp;
         }
 
-        return res;
+        return productQueries;
 
     }
     public static boolean checkBit(int i,int n){
-        return ((n&(1<<i))!=0);
+        return (n&1<<i)!=0;
     }
 }
