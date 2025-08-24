33fe3d1d379d81e0410df8af6a145c9b28441e95
Konda Sridhar <133839330+sridharkonda628@users.noreply.github.com>
2025-08-24 10:19:04 +0530

    Time: 1260 ms (5.33%), Space: 57.5 MB (6.82%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int cnt =0;
+        int len = nums.length;
+        int arr[] = new int[len];
+        int maxi =0;
+        boolean flg =false;
+
+        for(int i=0;i<len;i++){
+            if(nums[i]==1){
+                cnt++;
+                maxi = Math.max(cnt,maxi);
+                arr[i] = cnt;
+
+            }else{
+                cnt=0;
+                flg = true;
+                
+            }
+        }
+
+        if(arr[len-1]==len){
+            return len-1;
+        }
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+        int temp = arr[len-1];
+        for(int i=len-1;i>0;i--){
+            if(arr[i]!=0){
+                arr[i] = temp;
+            }else{
+                temp = arr[i-1];
+            }
+        }
+
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+        int ans = 0 ;
+        for(int i=1;i<len-1;i++){
+            if(nums[i]==0){
+                if(nums[i-1]==1 && nums[i+1]==1){
+                    ans = Math.max(ans,arr[i-1]+arr[i+1]);
+                }
+            }
+        }
+        // if(flg){
+            return Math.max(ans,maxi);
+        // }
+        // return M;
+    }
+}
